40b0e4e6dcd4d83924d6f5ec11f0b0c25d74ffef
Markus Scherer <markus.f.scherer@gmail.com>
2016-02-05 17:30:20 +0100

    Remove duplicate code by template magic.

--- src/policies/accumulating_result_handling_policy.hpp
+++ src/policies/accumulating_result_handling_policy.hpp
@@ -2,6 +2,7 @@
 #define ACCUMULATING_RESULT_HANDLING_POLICY_HPP_MSDA
 
 #include <cstdint>
+#include <type_traits>
 
 #include "emmintrin.h"
 #include "immintrin.h"
@@ -10,9 +11,7 @@
 #include "brute_force_evaluation_policy.hpp"
 
 template <uint64_t D, uint64_t A1, uint64_t A2>
-struct accumulating_result_handling_policy {};
-
-template <> struct accumulating_result_handling_policy<4, 3, 3> {
+struct accumulating_result_handling_policy_34_4_impl {
     struct registers {
         __m128i accmatfh = _mm_setzero_si128();
         __m128i accmatfl = _mm_setzero_si128();
@@ -42,7 +41,7 @@ template <> struct accumulating_result_handling_policy<4, 3, 3> {
         __m128i matfh = _mm_setzero_si128();
         __m128i matfl = _mm_setzero_si128();
         result_to_matrix(resf, matfl, matfh, c.shuf128, c.const2020, c.constFFFF,
-                         c.const3333);
+                         A2 == 3 ? c.const3333 : c.constFFFF);
 
         r.accmatfh = _mm_bslli_si128(r.accmatfh, 1);
         r.accmatfh = _mm_or_si128(r.accmatfh, matfh);
@@ -52,7 +51,7 @@ template <> struct accumulating_result_handling_policy<4, 3, 3> {
         __m128i matgh = _mm_setzero_si128();
         __m128i matgl = _mm_setzero_si128();
         result_to_matrix(resg, matgl, matgh, c.shuf128, c.const2020, c.constFFFF,
-                         c.const3333);
+                         A1 == 3 ? c.const3333 : c.constFFFF);
         r.accmatgh = _mm_bslli_si128(r.accmatgh, 1);
         r.accmatgh = _mm_or_si128(r.accmatgh, matgh);
 
@@ -60,9 +59,9 @@ template <> struct accumulating_result_handling_policy<4, 3, 3> {
         r.accmatgl = _mm_or_si128(r.accmatgl, matgl);
 
         if (matcount % 16 == 12) {
-            brute_force_evaluation_policy<4, 3>::eval(
+            brute_force_evaluation_policy<D, A1>::eval(
                 resf, matcount, r.accmatgl, r.accmatgh, ep1_reg, ep1_constants);
-            brute_force_evaluation_policy<4, 3>::eval(
+            brute_force_evaluation_policy<D, A2>::eval(
                 resg, matcount, r.accmatfl, r.accmatfh, ep2_reg, ep2_constants);
 
             int funceq = _mm_movemask_epi8(_mm_cmpeq_epi8(resg, resf));
@@ -108,191 +107,25 @@ private:
     }
 };
 
-template <> struct accumulating_result_handling_policy<4, 3, 4> {
-    struct registers {
-        __m128i accmatfh = _mm_setzero_si128();
-        __m128i accmatfl = _mm_setzero_si128();
-        __m128i accmatgh = _mm_setzero_si128();
-        __m128i accmatgl = _mm_setzero_si128();
-    };
+template <uint64_t D, uint64_t A1, uint64_t A2>
+struct accumulating_result_handling_policy {
+    typedef typename std::conditional<
+    D == 4 && ((A1 == 3 && A2 == 3) || (A1 == 3 && A2 == 4) ||
+               (A1 == 4 && A2 == 4)),
+               accumulating_result_handling_policy_34_4_impl<D, A1, A2>,
+               std::nullptr_t>::type Implementation;
 
-    struct constants {
-        const __m128i shuf128;
-        const __m128i const2020;
-        const __m128i const3333;
-        const __m128i constFFFF;
-
-        constants()
-            : shuf128(_mm_set_epi8(15, 11, 7, 3, 14, 10, 6, 2, 13, 9, 5, 1, 12, 8,
-                                   4, 0)),
-              const2020(_mm_set1_epi64x(0x0000000200000000)),
-              const3333(_mm_set1_epi32(0x03)), constFFFF(_mm_set1_epi32(0x0F)) {
-        }
-    };
+    typedef typename Implementation::registers registers;
+    typedef typename Implementation::constants constants;
 
     template <class R1, class R2, class C1, class C2>
     inline static bool handle_results(__m128i resf, __m128i resg,
                                       uint64_t matcount, R1& ep1_reg, R2& ep2_reg,
                                       C1& ep1_constants, C2& ep2_constants,
                                       registers& r, const constants& c) {
-        __m128i matfh = _mm_setzero_si128();
-        __m128i matfl = _mm_setzero_si128();
-        result_to_matrix(resf, matfl, matfh, c.shuf128, c.const2020, c.constFFFF,
-                         c.constFFFF);
-
-        r.accmatfh = _mm_bslli_si128(r.accmatfh, 1);
-        r.accmatfh = _mm_or_si128(r.accmatfh, matfh);
-
-        r.accmatfl = _mm_bslli_si128(r.accmatfl, 1);
-        r.accmatfl = _mm_or_si128(r.accmatfl, matfl);
-        __m128i matgh = _mm_setzero_si128();
-        __m128i matgl = _mm_setzero_si128();
-        result_to_matrix(resg, matgl, matgh, c.shuf128, c.const2020, c.constFFFF,
-                         c.const3333);
-        r.accmatgh = _mm_bslli_si128(r.accmatgh, 1);
-        r.accmatgh = _mm_or_si128(r.accmatgh, matgh);
-
-        r.accmatgl = _mm_bslli_si128(r.accmatgl, 1);
-        r.accmatgl = _mm_or_si128(r.accmatgl, matgl);
-
-        if (matcount % 16 == 12) {
-            brute_force_evaluation_policy<4, 3>::eval(
-                resf, matcount, r.accmatgl, r.accmatgh, ep1_reg, ep1_constants);
-            brute_force_evaluation_policy<4, 4>::eval(
-                resg, matcount, r.accmatfl, r.accmatfh, ep2_reg, ep2_constants);
-
-            int funceq = _mm_movemask_epi8(_mm_cmpeq_epi8(resg, resf));
-
-            if (funceq != 0xFFFF) {
-                return false;
-            }
-
-            r.accmatfh = _mm_setzero_si128();
-            r.accmatfl = _mm_setzero_si128();
-            r.accmatgh = _mm_setzero_si128();
-            r.accmatgl = _mm_setzero_si128();
-        }
-
-        return true;
-    }
-
-private:
-    inline static void result_to_matrix(const __m128i& res, __m128i& matl,
-                                        __m128i& math, const __m128i& shuf128,
-                                        const __m128i& const2020,
-                                        const __m128i& const_matl_mask,
-                                        const __m128i& const_math_mask) {
-        // unite neighbouring 2-bit-integers to 4-integers
-        // afterwards every even 8-bit-field field of math contains a valid
-        // 4-bit integer
-        math = _mm_shuffle_epi8(res, shuf128);
-#ifdef __AVX2__
-        math = _mm_sllv_epi32(math, const2020);
-#else
-        __m128i tmp = _mm_slli_epi32(math, 2);
-        math = _mm_blend_epi16(math, tmp, 0xCC);
-#endif
-        math = _mm_shuffle_epi8(math, shuf128);
-        math = _mm_srli_epi32(math, 8);
-        math = _mm_or_si128(math, res);
-
-        // write fields 0,4,8 and 12 to matl
-        matl = _mm_and_si128(math, const_matl_mask);
-        // write fields 2,6,10 and 14 to math (at 0,4,8 and 12)
-        math = _mm_srli_epi32(math, 16);
-        math = _mm_and_si128(math, const_math_mask);
-    }
-};
-
-template <> struct accumulating_result_handling_policy<4, 4, 4> {
-    struct registers {
-        __m128i accmatfh = _mm_setzero_si128();
-        __m128i accmatfl = _mm_setzero_si128();
-        __m128i accmatgh = _mm_setzero_si128();
-        __m128i accmatgl = _mm_setzero_si128();
-    };
-
-    struct constants {
-        const __m128i shuf128;
-        const __m128i const2020;
-        const __m128i constFFFF;
-
-        constants()
-            : shuf128(_mm_set_epi8(15, 11, 7, 3, 14, 10, 6, 2, 13, 9, 5, 1, 12, 8,
-                                   4, 0)),
-              const2020(_mm_set1_epi64x(0x0000000200000000)),
-              constFFFF(_mm_set1_epi32(0x0F)) {
-        }
-    };
-
-    template <class R1, class R2, class C1, class C2>
-    inline static bool handle_results(__m128i resf, __m128i resg,
-                                      uint64_t matcount, R1& ep1_reg, R2& ep2_reg,
-                                      C1& ep1_constants, C2& ep2_constants,
-                                      registers& r, const constants& c) {
-        __m128i matfh = _mm_setzero_si128();
-        __m128i matfl = _mm_setzero_si128();
-        result_to_matrix(resf, matfl, matfh, c.shuf128, c.const2020, c.constFFFF);
-
-        r.accmatfh = _mm_bslli_si128(r.accmatfh, 1);
-        r.accmatfh = _mm_or_si128(r.accmatfh, matfh);
-
-        r.accmatfl = _mm_bslli_si128(r.accmatfl, 1);
-        r.accmatfl = _mm_or_si128(r.accmatfl, matfl);
-        __m128i matgh = _mm_setzero_si128();
-        __m128i matgl = _mm_setzero_si128();
-        result_to_matrix(resg, matgl, matgh, c.shuf128, c.const2020, c.constFFFF);
-        r.accmatgh = _mm_bslli_si128(r.accmatgh, 1);
-        r.accmatgh = _mm_or_si128(r.accmatgh, matgh);
-
-        r.accmatgl = _mm_bslli_si128(r.accmatgl, 1);
-        r.accmatgl = _mm_or_si128(r.accmatgl, matgl);
-
-        if (matcount % 16 == 12) {
-            brute_force_evaluation_policy<4, 4>::eval(
-                resf, matcount, r.accmatgl, r.accmatgh, ep1_reg, ep1_constants);
-            brute_force_evaluation_policy<4, 4>::eval(
-                resg, matcount, r.accmatfl, r.accmatfh, ep2_reg, ep2_constants);
-
-            int funceq = _mm_movemask_epi8(_mm_cmpeq_epi8(resg, resf));
-
-            if (funceq != 0xFFFF) {
-                return false;
-            }
-
-            r.accmatfh = _mm_setzero_si128();
-            r.accmatfl = _mm_setzero_si128();
-            r.accmatgh = _mm_setzero_si128();
-            r.accmatgl = _mm_setzero_si128();
-        }
-
-        return true;
-    }
-
-private:
-    inline static void result_to_matrix(const __m128i& res, __m128i& matl,
-                                        __m128i& math, const __m128i& shuf128,
-                                        const __m128i& const2020,
-                                        const __m128i& constFFFF) {
-        // unite neighbouring 2-bit-integers to 4-integers
-        // afterwards every even 8-bit-field field of math contains a valid
-        // 4-bit integer
-        math = _mm_shuffle_epi8(res, shuf128);
-#ifdef __AVX2__
-        math = _mm_sllv_epi32(math, const2020);
-#else
-        __m128i tmp = _mm_slli_epi32(math, 2);
-        math = _mm_blend_epi16(math, tmp, 0xCC);
-#endif
-        math = _mm_shuffle_epi8(math, shuf128);
-        math = _mm_srli_epi32(math, 8);
-        math = _mm_or_si128(math, res);
-
-        // write fields 0,4,8 and 12 to matl
-        matl = _mm_and_si128(math, constFFFF);
-        // write fields 2,6,10 and 14 to math (at 0,4,8 and 12)
-        math = _mm_srli_epi32(math, 16);
-        math = _mm_and_si128(math, constFFFF);
+        return Implementation::handle_results(resf, resg, matcount, ep1_reg,
+                                              ep2_reg, ep1_constants, ep2_constants,
+                                              r, c);
     }
 };
 
--- src/simd_solving_policy.hpp
+++ src/simd_solving_policy.hpp
@@ -28,193 +28,81 @@ template <uint64_t D, uint64_t A1, uint64_t A2,
           class ElementType, class MatrixGenerationPolicy,
           class TransposedMatrixGenerationPolicy, class EvaluationPolicy1,
           class EvaluationPolicy2, class ResultsHandlingPolicy>
+struct simd_solving_policy_34_4_impl {
+    typedef ElementType element_type;
+    static const uint64_t domain_size = D;
+    static const uint64_t cell_count = A1 * A2;
+    static const uint64_t matrix_count = cpow(domain_size, cell_count);
+
+    inline static bool commutes(FunctionType<D, A1, ElementType> f,
+                                FunctionType<D, A2, ElementType> g) {
+
+        const typename MatrixGenerationPolicy::constants mgp_constants;
+        const typename TransposedMatrixGenerationPolicy::constants tmgp_constants;
+        const typename EvaluationPolicy1::constants ep1_constants;
+        const typename EvaluationPolicy2::constants ep2_constants;
+        const typename ResultsHandlingPolicy::constants rhp_constants;
+
+        typename EvaluationPolicy1::registers ep1_reg;
+        typename EvaluationPolicy2::registers ep2_reg;
+        typename ResultsHandlingPolicy::registers rhp_reg;
+
+        EvaluationPolicy1::init_registers(ep1_reg, f);
+        EvaluationPolicy2::init_registers(ep2_reg, g);
+
+        __m128i resf;
+        __m128i resg;
+
+        __m128i math;
+        __m128i matl;
+
+        __m128i tmath;
+        __m128i tmatl;
+
+        MatrixGenerationPolicy::init_matrix(matl, math);
+        TransposedMatrixGenerationPolicy::init_matrix(tmatl, tmath);
+
+        for (uint64_t i = 0; i < matrix_count; i += 4) {
+            MatrixGenerationPolicy::next_matrix(i, matl, math, mgp_constants);
+            TransposedMatrixGenerationPolicy::next_matrix(i, tmatl, tmath,
+                                                          tmgp_constants);
+
+            EvaluationPolicy1::eval(resf, i, matl, math, ep1_reg, ep1_constants);
+            EvaluationPolicy2::eval(resg, i, tmatl, tmath, ep2_reg, ep2_constants);
+
+            if (!ResultsHandlingPolicy::handle_results(
+                        resf, resg, i, ep1_reg, ep2_reg, ep1_constants, ep2_constants,
+                        rhp_reg, rhp_constants)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+template <uint64_t D, uint64_t A1, uint64_t A2,
+          template <uint64_t, uint64_t, typename> class FunctionType,
+          class ElementType, class MatrixGenerationPolicy,
+          class TransposedMatrixGenerationPolicy, class EvaluationPolicy1,
+          class EvaluationPolicy2, class ResultsHandlingPolicy>
 struct simd_solving_policy {
     typedef ElementType element_type;
     static const uint64_t domain_size = D;
     static const uint64_t cell_count = A1 * A2;
     static const uint64_t matrix_count = cpow(domain_size, cell_count);
 
+    typedef typename std::conditional<
+    D == 4 && ((A1 == 3 && A2 == 3) || (A1 == 3 && A2 == 4) ||
+               (A1 == 4 && A2 == 4)),
+               simd_solving_policy_34_4_impl<
+               D, A1, A2, FunctionType, ElementType, MatrixGenerationPolicy,
+               TransposedMatrixGenerationPolicy, EvaluationPolicy1,
+               EvaluationPolicy2, ResultsHandlingPolicy>,
+               std::nullptr_t>::type Implementation;
+
     static bool commutes(FunctionType<D, A1, ElementType> f1,
-                         FunctionType<D, A2, ElementType> f2);
-};
-
-template <template <uint64_t, uint64_t, typename> class FunctionType,
-          class ElementType, class MatrixGenerationPolicy,
-          class TransposedMatrixGenerationPolicy, class EvaluationPolicy1,
-          class EvaluationPolicy2, class ResultsHandlingPolicy>
-struct simd_solving_policy<4, 3, 3, FunctionType, ElementType,
-           MatrixGenerationPolicy,
-           TransposedMatrixGenerationPolicy, EvaluationPolicy1,
-           EvaluationPolicy2, ResultsHandlingPolicy> {
-    typedef ElementType element_type;
-    static const uint64_t domain_size = 4;
-    static const uint64_t cell_count = 3 * 3;
-    static const uint64_t matrix_count = cpow(domain_size, cell_count);
-
-    static bool commutes(FunctionType<4, 3, ElementType> f,
-                         FunctionType<4, 3, ElementType> g) {
-
-        const typename MatrixGenerationPolicy::constants mgp_constants;
-        const typename TransposedMatrixGenerationPolicy::constants tmgp_constants;
-        const typename EvaluationPolicy1::constants ep1_constants;
-        const typename EvaluationPolicy2::constants ep2_constants;
-        const typename ResultsHandlingPolicy::constants rhp_constants;
-
-        typename EvaluationPolicy1::registers ep1_reg;
-        typename EvaluationPolicy2::registers ep2_reg;
-        typename ResultsHandlingPolicy::registers rhp_reg;
-
-        EvaluationPolicy1::init_registers(ep1_reg, f);
-        EvaluationPolicy2::init_registers(ep2_reg, g);
-
-        __m128i resf;
-        __m128i resg;
-
-        __m128i math;
-        __m128i matl;
-
-        __m128i tmath;
-        __m128i tmatl;
-
-        MatrixGenerationPolicy::init_matrix(matl, math);
-        TransposedMatrixGenerationPolicy::init_matrix(tmatl, tmath);
-
-        for (uint64_t i = 0; i < matrix_count; i += 4) {
-            MatrixGenerationPolicy::next_matrix(i, matl, math, mgp_constants);
-            TransposedMatrixGenerationPolicy::next_matrix(i, tmatl, tmath,
-                                                          tmgp_constants);
-
-            EvaluationPolicy1::eval(resf, i, matl, math, ep1_reg, ep1_constants);
-            EvaluationPolicy2::eval(resg, i, tmatl, tmath, ep2_reg, ep2_constants);
-
-            if (!ResultsHandlingPolicy::handle_results(
-                        resf, resg, i, ep1_reg, ep2_reg, ep1_constants, ep2_constants,
-                        rhp_reg, rhp_constants)) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-};
-
-template <template <uint64_t, uint64_t, typename> class FunctionType,
-          class ElementType, class MatrixGenerationPolicy,
-          class TransposedMatrixGenerationPolicy, class EvaluationPolicy1,
-          class EvaluationPolicy2, class ResultsHandlingPolicy>
-struct simd_solving_policy<4, 3, 4, FunctionType, ElementType,
-           MatrixGenerationPolicy,
-           TransposedMatrixGenerationPolicy, EvaluationPolicy1,
-           EvaluationPolicy2, ResultsHandlingPolicy> {
-    typedef ElementType element_type;
-    static const uint64_t domain_size = 4;
-    static const uint64_t cell_count = 3 * 4;
-    static const uint64_t matrix_count = cpow(domain_size, cell_count);
-
-    static bool commutes(FunctionType<4, 3, ElementType> f,
-                         FunctionType<4, 4, ElementType> g) {
-
-        const typename MatrixGenerationPolicy::constants mgp_constants;
-        const typename TransposedMatrixGenerationPolicy::constants tmgp_constants;
-        const typename EvaluationPolicy1::constants ep1_constants;
-        const typename EvaluationPolicy2::constants ep2_constants;
-        const typename ResultsHandlingPolicy::constants rhp_constants;
-
-        typename EvaluationPolicy1::registers ep1_reg;
-        typename EvaluationPolicy2::registers ep2_reg;
-        typename ResultsHandlingPolicy::registers rhp_reg;
-
-        EvaluationPolicy1::init_registers(ep1_reg, f);
-        EvaluationPolicy2::init_registers(ep2_reg, g);
-
-        __m128i resf;
-        __m128i resg;
-
-        __m128i math;
-        __m128i matl;
-
-        __m128i tmath;
-        __m128i tmatl;
-
-        MatrixGenerationPolicy::init_matrix(matl, math);
-        TransposedMatrixGenerationPolicy::init_matrix(tmatl, tmath);
-
-        for (uint64_t i = 0; i < matrix_count; i += 4) {
-            MatrixGenerationPolicy::next_matrix(i, matl, math, mgp_constants);
-            TransposedMatrixGenerationPolicy::next_matrix(i, tmatl, tmath,
-                                                          tmgp_constants);
-
-            EvaluationPolicy1::eval(resf, i, matl, math, ep1_reg, ep1_constants);
-            EvaluationPolicy2::eval(resg, i, tmatl, tmath, ep2_reg, ep2_constants);
-
-            if (!ResultsHandlingPolicy::handle_results(
-                        resf, resg, i, ep1_reg, ep2_reg, ep1_constants, ep2_constants,
-                        rhp_reg, rhp_constants)) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-};
-
-template <template <uint64_t, uint64_t, typename> class FunctionType,
-          class ElementType, class MatrixGenerationPolicy,
-          class TransposedMatrixGenerationPolicy, class EvaluationPolicy1,
-          class EvaluationPolicy2, class ResultsHandlingPolicy>
-struct simd_solving_policy<4, 4, 4, FunctionType, ElementType,
-           MatrixGenerationPolicy,
-           TransposedMatrixGenerationPolicy, EvaluationPolicy1,
-           EvaluationPolicy2, ResultsHandlingPolicy> {
-    typedef ElementType element_type;
-    static const uint64_t domain_size = 4;
-    static const uint64_t cell_count = 4 * 4;
-    static const uint64_t matrix_count = cpow(domain_size, cell_count);
-
-    static bool commutes(FunctionType<4, 4, ElementType> f,
-                         FunctionType<4, 4, ElementType> g) {
-
-        const typename MatrixGenerationPolicy::constants mgp_constants;
-        const typename TransposedMatrixGenerationPolicy::constants tmgp_constants;
-        const typename EvaluationPolicy1::constants ep1_constants;
-        const typename EvaluationPolicy2::constants ep2_constants;
-        const typename ResultsHandlingPolicy::constants rhp_constants;
-
-        typename EvaluationPolicy1::registers ep1_reg;
-        typename EvaluationPolicy2::registers ep2_reg;
-        typename ResultsHandlingPolicy::registers rhp_reg;
-
-        EvaluationPolicy1::init_registers(ep1_reg, f);
-        EvaluationPolicy2::init_registers(ep2_reg, g);
-
-        __m128i resf;
-        __m128i resg;
-
-        __m128i math;
-        __m128i matl;
-
-        __m128i tmath;
-        __m128i tmatl;
-
-        MatrixGenerationPolicy::init_matrix(matl, math);
-        TransposedMatrixGenerationPolicy::init_matrix(tmatl, tmath);
-
-        for (uint64_t i = 0; i < matrix_count; i += 4) {
-            MatrixGenerationPolicy::next_matrix(i, matl, math, mgp_constants);
-            TransposedMatrixGenerationPolicy::next_matrix(i, tmatl, tmath,
-                                                          tmgp_constants);
-
-            EvaluationPolicy1::eval(resf, i, matl, math, ep1_reg, ep1_constants);
-            EvaluationPolicy2::eval(resg, i, tmatl, tmath, ep2_reg, ep2_constants);
-
-            if (!ResultsHandlingPolicy::handle_results(
-                        resf, resg, i, ep1_reg, ep2_reg, ep1_constants, ep2_constants,
-                        rhp_reg, rhp_constants)) {
-                return false;
-            }
-        }
-
-        return true;
-    }
+                         FunctionType<D, A2, ElementType> f2) {
+        return Implementation::commutes(f1, f2);
+    };
 };
 #endif
