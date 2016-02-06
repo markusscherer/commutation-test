f5a941a99bbed01319dbb806db19a75cb4654c72
Markus Scherer <markus.f.scherer@gmail.com>
2016-02-06 17:07:13 +0100

    Add arity combination 1/3 for incremental_matrix_generation.

--- src/policies/incremental_matrix_generation_policy.hpp
+++ src/policies/incremental_matrix_generation_policy.hpp
@@ -40,6 +40,37 @@ template <> struct incremental_matrix_generation_policy<4, 2, 2> {
     }
 };
 
+template <> struct incremental_matrix_generation_policy<4, 1, 3> {
+    struct constants {
+        const __m128i const0010;
+        const __m128i const01_40;
+
+        constants()
+            : const0010(
+                  _mm_set_epi8(0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0)),
+              const01_40(_mm_set_epi8(0, 1, -4, 0, 0, 1, -4, 0, 0, 1, -4, 0, 0, 1,
+                                      -4, 0)) {
+        }
+    };
+
+    inline static void init_matrix(__m128i& matl, __m128i& math) {
+        const constants c;
+        next_matrix(0, matl, math, c);
+        matl = _mm_sub_epi8(_mm_setzero_si128(), matl);
+        math = _mm_sub_epi8(_mm_setzero_si128(), math);
+        matl = _mm_add_epi8(matl, _mm_set_epi32(3, 2, 1, 0));
+    }
+
+    inline static void next_matrix(uint64_t matcount, __m128i& matl,
+                                   __m128i& math, const constants& c) {
+        matl = _mm_add_epi8(matl, c.const0010);
+
+        if (matcount % (1 << 4) == 0) {
+            matl = _mm_add_epi8(matl, c.const01_40);
+        }
+    }
+};
+
 template <> struct incremental_matrix_generation_policy<4, 2, 3> {
     struct constants {
         const __m128i const0004;
--- src/policies/incremental_transposed_matrix_generation_policy.hpp
+++ src/policies/incremental_transposed_matrix_generation_policy.hpp
@@ -48,6 +48,40 @@ template <> struct incremental_transposed_matrix_generation_policy<4, 2, 2> {
     }
 };
 
+template <> struct incremental_transposed_matrix_generation_policy<4, 1, 3> {
+    struct constants {
+        const __m128i const0001;
+        const __m128i const0004;
+        const __m128i const000_16;
+
+        constants()
+            : const0001(_mm_set_epi32(1, 1, 1, 1)),
+              const0004(
+                  _mm_set_epi8(0, 0, 0, 4, 0, 0, 0, 4, 0, 0, 0, 4, 0, 0, 0, 4)),
+              const000_16(_mm_set_epi8(0, 0, 0, -16, 0, 0, 0, -16, 0, 0, 0, -16, 0,
+                                       0, 0, -16)) {
+        }
+    };
+
+    inline static void init_matrix(__m128i& matl, __m128i& math) {
+        const constants c;
+        next_matrix(0, matl, math, c);
+        matl = _mm_sub_epi8(_mm_setzero_si128(), matl);
+        math = _mm_sub_epi8(_mm_setzero_si128(), math);
+        matl = _mm_add_epi8(matl, _mm_set_epi32(3, 2, 1, 0));
+    }
+
+    inline static void next_matrix(uint64_t matcount, __m128i& matl,
+                                   __m128i& math, const constants& c) {
+        matl = _mm_add_epi8(matl, c.const0004);
+
+        if (matcount % (1 << 4) == 0) {
+            matl = _mm_add_epi8(matl, c.const000_16);
+            math = _mm_add_epi8(math, c.const0001);
+        }
+    }
+};
+
 template <> struct incremental_transposed_matrix_generation_policy<4, 2, 3> {
     struct constants {
         const __m128i const0001;
