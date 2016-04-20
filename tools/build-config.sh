820a8e4bfacf6185c7899b771ae8c1d2e62c31d8
Markus Scherer <markus.f.scherer@gmail.com>
2016-04-20 14:54:28 +0200

    Added compiler to name of output.

--- tools/build-config.sh
+++ tools/build-config.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 conf_dir=$(dirname $1)
-name=comtest_$(basename $conf_dir)
 
 outdir=bin/
 
 if [[ -z $CC ]]; then
   CC=g++
 fi
+name=$CC""_comtest_$(basename $conf_dir)
+
 
 options='-std=c++11 -Wall -Wpedantic -msse4.1 -O3'
 
